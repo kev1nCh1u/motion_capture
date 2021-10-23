522fda47adc7c5549411b0e81a0cf062e9c4a5e0
kevin <fsd83811@gmail.com>
2021-10-23 11:26:01 +0800

    git rm -rf --cached .

---
+++ README.md
@@ -0,0 +1,4 @@
+# kevin_motion_capture
+
+## run
+    python src/dahua_python/src/ros_cv_getFrame.py
---
+++ setup.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# Program:
+#       This program start kevin_motion_capture
+# History:
+# 2021/10/23	kevin	First release
+
+pathVar=$(pwd)
+echo "\nNow path:"
+echo $pathVar
+
+startPath="${pathVar}/src/kevin_start/src"
+echo "\nStart path:"
+echo $startPath
+
+PATH=$PATH:$startPath
+echo "\nNew system path:"
+echo $PATH
