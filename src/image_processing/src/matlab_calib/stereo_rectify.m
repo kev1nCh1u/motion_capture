f5da29080f75a41cb5ba809cc35691eba7f21cb6
kevin <fsd83811@gmail.com>
2021-10-24 14:50:04 +0800

    matlab

---
+++ src/image_processing/src/matlab_calib/stereo_rectify.m
@@ -0,0 +1,18 @@
+I1 = imread('/home/kevin/src/opencv_practice/img/stereo_calibration/new/1/01.jpg');
+I2 = imread('/home/kevin/src/opencv_practice/img/stereo_calibration/new/2/01.jpg');
+
+[J1, J2] = rectifyStereoImages(I1, I2, stereoParams);
+cat_j = cat(2,J1,J2);
+figure; imshow(cat_j);
+
+cat_j_size = size(cat_j);
+gap = 27;
+for i = gap:gap:cat_j_size(1)
+    line([0 cat_j_size(2),], [i i]);
+end
+
+[J1, J2] = rectifyStereoImages(I1, I2, stereoParams, 'OutputView','full');
+Anaglyph = stereoAnaglyph(J1,J2);
+figure; imshow(Anaglyph)
+
+imtool(Anaglyph);
