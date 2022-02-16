8e1edce9beb46f12b6c9f2a0bf5db903aad1cfe9
KevinChiu <fs83811@gmail.com>
2022-02-16 14:25:40 +0800

    chess_depth_tri

--- src/image_processing/calibration/stereo_board_depth_triangulation.py
+++ src/image_processing/calibration/stereo_board_depth_triangulation.py
@@ -7,61 +7,55 @@ from matplotlib import pyplot as plt
 import time
 import argparse
 
-###############################################################################################
-# find_depth
-###############################################################################################
+###################################################################################
+# triangulate
+###################################################################################
+def triangulate(cameraMatrix1, cameraMatrix2, RotationOfCamera2, TranslationOfCamera2, point_x, point_x_):
+    # p
+    k = cameraMatrix1
+    rt =  np.eye(3,4)
+    p = np.dot(k, rt)
 
+    # pp
+    k_ = cameraMatrix2
+    rt_ = np.concatenate((RotationOfCamera2, TranslationOfCamera2), axis=1)
+    p_ = np.dot(k_, rt_)
+    
+    # point
+    u = point_x[0]
+    v = point_x[1]
+    u_ = point_x_[0]
+    v_ = point_x_[1]
+    # print("uv:", u, v, u_, v_)
 
-def find_depth(left_point, right_point, baseline, focal):
-    x_right = right_point[0]
-    x_left = left_point[0]
+    # equations
+    A = np.array([
+        u * p[2] - p[0],
+        v * p[2] - p[1],
+        u_ * p_[2] - p_[0],
+        v_ * p_[2] - p_[1]], dtype='float64'
+    )
 
-    # displacement between left and right frames [pixels]
-    disparity = abs(x_left - x_right)
-    zDepth = (baseline * focal) / disparity            # z depth in [mm]
+    # svd
+    U, S, V = np.linalg.svd(A)
+    X = V.transpose()[:, -1]
+    print("X:", X)
+    X = X / X[3]
+    print("world_point:", X)
+    return X[0:3]
 
-    return zDepth
-
-###############################################################################################
-# calcu_world_point
-###############################################################################################
-
-
-def calcu_world_point(point, z_depth, focal):
-    world_points = np.zeros((3), np.float64)
-    x_cam = point[0]
-    y_cam = point[1]
-    world_points[0] = (x_cam * z_depth) / focal
-    world_points[1] = (y_cam * z_depth) / focal
-    world_points[2] = z_depth
-    return world_points
-
-###############################################################################################
-# undistortRectify remap
-###############################################################################################
-
-
-def undistortRectify(stereoMapL_x, stereoMapL_y, stereoMapR_x, stereoMapR_y, frameL, frameR):
-
-    # Undistort and rectify images
-    undistortedL = cv2.remap(
-        frameL, stereoMapL_x, stereoMapL_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
-    undistortedR = cv2.remap(
-        frameR, stereoMapR_x, stereoMapR_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
-
-    return undistortedL, undistortedR
 
 
 ###################################################################################
 # main
 ###################################################################################
 def main():
-    # kevin args
+    ######################################## kevin args
     parser = argparse.ArgumentParser()
     parser.add_argument("-id", "--image_id", default='04', help="01~21")
     args = parser.parse_args()
 
-    # load yaml param
+    ########################################## load yaml param
     fs = cv2.FileStorage(
         "data/param/matlab_stereo_param.yaml", cv2.FILE_STORAGE_READ)
 
@@ -84,39 +78,22 @@ def main():
     distCoeffs2 = np.concatenate(
         (RadialDistortion2, TangentialDistortion2), axis=1).astype('float64')
     imageSize = ImageSize.ravel()[::-1].astype('int64')
-    stereoR = np.transpose(RotationOfCamera2).astype('float64')
-    stereoT = np.transpose(TranslationOfCamera2).astype('float64')
+    RotationOfCamera2 = np.transpose(RotationOfCamera2).astype('float64')
+    TranslationOfCamera2 = np.transpose(TranslationOfCamera2).astype('float64')
 
     print('\n cameraMatrix1\n', cameraMatrix1)
     print('\n distCoeffs1\n', distCoeffs1)
     print('\n cameraMatrix2\n', cameraMatrix2)
     print('\n distCoeffs2\n', distCoeffs2)
     print('\n imageSize\n', imageSize)
-    print('\n stereoR\n', stereoR)
-    print('\n stereoT\n', stereoT)
+    print('\n RotationOfCamera2\n', RotationOfCamera2)
+    print('\n TranslationOfCamera2\n', TranslationOfCamera2)
     print()
 
     while 1:
+        ########################################## get frame
         capFlag = 0
         save_path = 'img/result/'
-
-        # Camera parameters to undistort and rectify images
-        cv_file = cv2.FileStorage()
-        cv_file.open('data/param/stereoMap.xml', cv2.FileStorage_READ)
-        stereoMapL_x = cv_file.getNode('stereoMapL_x').mat()
-        stereoMapL_y = cv_file.getNode('stereoMapL_y').mat()
-        stereoMapR_x = cv_file.getNode('stereoMapR_x').mat()
-        stereoMapR_y = cv_file.getNode('stereoMapR_y').mat()
-
-        # Stereo vision setup parameters
-        fs = cv2.FileStorage(
-            "data/param/matlab_stereo_param.yaml", cv2.FILE_STORAGE_READ)
-        baseline = abs(fs.getNode("TranslationOfCamera2").mat().ravel()[0])
-        focalLength = fs.getNode("FocalLength").mat().ravel()[0]
-
-        # termination criteria for cornerSubPix
-        criteria = (cv2.TERM_CRITERIA_EPS +
-                    cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
         # Open both cameras
         if capFlag:
@@ -141,9 +118,7 @@ def main():
         # cv2.imshow('frame_right',frame_right)
         # cv2.waitKey(0)
 
-        # triangulation depth
-        print("triangulation_depth ========================================")
-
+        ########################################## find point
         # Convert the BGR image to gray
         gray_left = cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY)
         gray_right = cv2.cvtColor(frame_right, cv2.COLOR_BGR2GRAY)
@@ -155,6 +130,8 @@ def main():
             gray_right, (9, 6), None)
 
         # subpix
+        criteria = (cv2.TERM_CRITERIA_EPS +
+                    cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001) # termination criteria for cornerSubPix
         corners_left = cv2.cornerSubPix(
             gray_left, corners_left, (11, 11), (-1, -1), criteria)
         corners_right = cv2.cornerSubPix(
@@ -170,64 +147,46 @@ def main():
         center_point_right1 = corners_right[conerNum].ravel()
         print("center_point1:", center_point_left1, center_point_right1)
 
-        # tri p1
-        tri_k1 = cameraMatrix1
-        tri_rt1 = np.array([[1.0, 0.0, 0.0, 0.0],
-                            [0.0, 1.0, 0.0, 0.0],
-                            [0.0, 0.0, 1.0, 0.0]])
-        tri_p1 = np.dot(tri_k1, tri_rt1)
 
-        # tri p2
-        tri_k2 = cameraMatrix2
-        tri_rt2 = np.concatenate((stereoR, stereoT), axis=1)
-        tri_p2 = np.dot(tri_k2, tri_rt2)
 
-        # point0
-        u1 = center_point_left[0]
-        v1 = center_point_left[1]
-        u2 = center_point_right[0]
-        v2 = center_point_right[1]
-        print("uv:", u1, v1, u2, v2)
-
-        A = np.array([u1*tri_p1[2]-tri_p1[0],
-                      v1*tri_p1[2]-tri_p1[1],
-                      u2*tri_p2[2]-tri_p2[0],
-                      v2*tri_p2[2]-tri_p2[1]], dtype='float64')
-
-        U, sigma, VT = np.linalg.svd(A)
-        V = VT.transpose()
-        X = V[:, -1]
-        X = X / X[3]
-        print("world_point:", X)
+        ############################################# triangulate
+        print("triangulation_depth ========================================")
         world_points = np.zeros((2, 3), np.float64)
-        world_points[0] = X[0:3]
-
-        # point1
-        u1_1 = center_point_left1[0]
-        v1_1 = center_point_left1[1]
-        u2_1 = center_point_right1[0]
-        v2_1 = center_point_right1[1]
-        print("uv1", u1_1, v1_1, u2_1, v2_1)
-
-        A = np.array([u1_1*tri_p1[2]-tri_p1[0],
-                      v1_1*tri_p1[2]-tri_p1[1],
-                      u2_1*tri_p2[2]-tri_p2[0],
-                      v2_1*tri_p2[2]-tri_p2[1]], dtype='float64')
-
-        U, sigma, VT = np.linalg.svd(A)
-        V = VT.transpose()
-        X = V[:, -1]
-        X = X / X[3]
-        print("world_point1:", X)
-        world_points[1] = X[0:3]
+        world_points[0] = triangulate(cameraMatrix1, cameraMatrix2, RotationOfCamera2, TranslationOfCamera2, center_point_left, center_point_right)
+        world_points[1] = triangulate(cameraMatrix1, cameraMatrix2, RotationOfCamera2, TranslationOfCamera2, center_point_left1, center_point_right1)
 
         # find distance
         print("world_points:", world_points)
-        distance = ((world_points[0, 0] - world_points[1, 0])**2 + (world_points[0, 1] -
-                    world_points[1, 1])**2 + (world_points[0, 2] - world_points[1, 2])**2)**0.5
+        distance = ((world_points[0, 0] - world_points[1, 0])**2 
+                + (world_points[0, 1] - world_points[1, 1])**2 
+                + (world_points[0, 2] - world_points[1, 2])**2)**0.5
         print("distance:", distance)
-
         print()
+
+        #################################### cv draw picture
+        # draw point
+        cv2.circle(frame_left, center_point_left.astype(
+                np.int32), 5, (0, 0, 255), -1)
+        cv2.circle(frame_left, center_point_left1.astype(
+                np.int32), 5, (255, 0, 0), -1)
+        cv2.circle(frame_right, center_point_right.astype(
+                np.int32), 5, (0, 0, 255), -1)
+        cv2.circle(frame_right, center_point_right1.astype(
+                np.int32), 5, (255, 0, 0), -1)
+
+        # draw text
+        text = "X: " + str(round(world_points[0, 0], 2))
+        cv2.putText(frame_left, text,
+                    (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        text = "Y: " + str(round(world_points[0, 1], 2))
+        cv2.putText(frame_left, text,
+                    (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        text = "Depth: " + str(round(world_points[0, 2], 2))
+        cv2.putText(frame_left, text,
+                    (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        text = "Distance: " + str(round(distance, 2))
+        cv2.putText(frame_left, text,
+                    (10, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         # mix to show on one picture
         vis = np.concatenate((frame_left, frame_right), axis=1)
@@ -247,7 +206,7 @@ def main():
         # if s save image
         elif inputKey == ord('s'):
             current_time = time.time()  # catch time
-            filename = save_path + 'chess_vis_' + str(current_time) + '.jpg'
+            filename = save_path + 'chess_vis_tri_' + str(current_time) + '.jpg'
             cv2.imwrite(filename, vis)
             print('\nSave:', filename, '\n')
 
