dd53ce18c70bd2274c5e8477f1e081dd3cfd6fb3
KevinChiu <fs83811@gmail.com>
2022-07-21 17:32:56 +0800

    probe_circle_error

--- src/chart/circle_compare.py
+++ src/chart/circle_compare.py
@@ -85,22 +85,40 @@ print("center_ndi",center_ndi)
 center_t = center_ndi - center
 print("center_t",center_t)
 
-probe[:,1]
+point_ndi[:,8] = point_ndi[:,8]-center_t[0]
+point_ndi[:,9] = point_ndi[:,9]-center_t[1]
 
 ######################################################################
 # show all 3d
 ######################################################################
-fig = plt.figure()
-# ax = fig.gca(projection='3d')
-ax = fig.add_subplot(projection='3d')
-ax.set_xlabel('X(mm)')
-ax.set_ylabel('Y(mm)')
-ax.set_zlabel('Z(mm)')
-# sc = ax.scatter(point[:,1], point[:,2], point[:,3], s=20, label='Marker',)
-sc2 = ax.scatter(probe[:,0], probe[:,1], probe[:,2], s=20, label='Probe')
-# sc3 = ax.scatter(point_ndi[:,8]-center_t[0], point_ndi[:,9]-center_t[1], point_ndi[:,10], s=20, label='NDI', c=point_ndi[:,11], cmap='jet',vmin=0, vmax=3)
-ax.legend()
-plt.show()
+# fig = plt.figure()
+# # ax = fig.gca(projection='3d')
+# ax = fig.add_subplot(projection='3d')
+# ax.set_xlabel('X(mm)')
+# ax.set_ylabel('Y(mm)')
+# ax.set_zlabel('Z(mm)')
+# # sc = ax.scatter(point[:,1], point[:,2], point[:,3], s=20, label='Marker',)
+# sc2 = ax.scatter(probe[:,0], probe[:,1], probe[:,2], s=20, label='Probe')
+# # sc3 = ax.scatter(point_ndi[:,8]-center_t[0], point_ndi[:,9]-center_t[1], point_ndi[:,10], s=20, label='NDI', c=point_ndi[:,11], cmap='jet',vmin=0, vmax=3)
+# ax.legend()
+# plt.show()
+
+######################################################################
+# calculate error
+######################################################################
+dis = np.zeros((size))
+for i in range(size):
+    dis[i] = kevincv.euclideanDistances3d(center,probe[i,0:2])
+    # print("dis",dis[i])
+print("error_rmse:",kevincv.rmseFuc(50,dis))
+print("error_std:",np.std(dis))
+
+dis = np.zeros((size_ndi))
+for i in range(size_ndi):
+    dis[i] = kevincv.euclideanDistances3d(center,point_ndi[i,8:10])
+    # print("dis",dis[i])
+print("ndi_error_rmse:",kevincv.rmseFuc(65,dis))
+print("ndi_error_std:",np.std(dis))
 
 ######################################################################
 # show all 2d
@@ -108,22 +126,20 @@ plt.show()
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.set_xlabel('X(mm)')
-ax.set_ylabel('Z(mm)')
+ax.set_ylabel('Y(mm)')
 
 plt.gca().invert_yaxis()
 
 ax.add_patch(plt.Circle((center),50,color='r',alpha=1,fill=False))
 ax.add_patch(plt.Circle((center),65,color='r',alpha=1,fill=False))
+
 sc_ = ax.scatter(0, 0, s=20, color='r', label='True')
-# sc = ax.scatter(point[:,0], point[:,2], s=20, label='Marker')
 sc3 = ax.scatter(center[0], center[1], s=20, label='Center')
-sc2 = ax.scatter(probe[:,0], probe[:,1], s=20, label='Probe')
-sc4 = ax.scatter(point_ndi[:,8]-center_t[0], point_ndi[:,9]-center_t[1], s=20, label='NDI', c=point_ndi[:,11], cmap='jet',vmin=0, vmax=3)
-# ax.set_title('Distance '+ str(round(point[0,3]))+ "mm")
+# sc = ax.scatter(point[:,0], point[:,2], s=20, label='Marker')
+sc2 = ax.scatter(probe[:,0], probe[:,1], s=20, label='Our')
+sc4 = ax.scatter(point_ndi[:,8], point_ndi[:,9], s=20, label='NDI', c=point_ndi[:,11], cmap='jet',vmin=0, vmax=3)
+ax.set_title('Probe circle test ')
+# ax.text(2.0,9.5,"qqqqqq",fontsize=14)
 ax.legend()
 plt.axis('scaled')
 plt.show()
-
-######################################################################
-# calculate error
-######################################################################
