5d73def6bc53fd8c0c3f3fb39b4a5bd558b51ce8
KevinChiu <fs83811@gmail.com>
2022-07-25 21:35:07 +0800

    ndi

--- src/chart/ndi_rom.py
+++ src/chart/ndi_rom.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 import numpy as np
 
-df = pd.read_csv("data/result/ndi/walk_50.csv", header=0)
+df = pd.read_csv("data/result/ndi/walk_30.csv", header=0)
 # df = pd.read_csv("data/result/point_main_robot_rom_2.csv", header=0)
 # df = pd.read_csv("data/result/point_main_human_rom_30.csv", header=0)
 point = df.to_numpy()
 # print(point[0])
 
-point = point[3:200]
+point = point[100:700]
 
 point0 = point[:,0:29]
 minYaw0 = np.min(point0[:,6])
@@ -21,7 +21,7 @@ minYaw1 = np.min(point1[:,6])
 maxYaw1 = np.max(point1[:,6])
 print("minYaw1:",minYaw1,"maxYaw1:",maxYaw1)
 
-romAngle = np.abs(point0[:,6]) - np.abs(point1[:,6])
+romAngle = np.abs(point1[:,6]) - np.abs(point0[:,6])
 minYawRom = np.min(romAngle)
 maxYawRom = np.max(romAngle)
 print("minYawRom",minYawRom,"maxYawRom",maxYawRom)
@@ -43,7 +43,7 @@ fig, ax = plt.subplots()
 # ax.plot(num, point1[:,4], '-', label='Roll1')
 # ax.plot(num, point1[:,5], '-', label='Pitch1')
 # ax.plot(num, point1[:,6], '-', label='Yaw1')
-ax.plot(num/20, romAngle, '-', label='ROM')
+ax.plot(num/60, romAngle, '-', label='ROM')
 
 ax.legend()
 
@@ -52,9 +52,9 @@ ax.set_ylabel('Angle(deg)')
 ax.set_title('Angle ROM')
 
 
-x1, y1 = [0, num[-1]/20], [minYawRom, minYawRom]
-x2, y2 = [0, num[-1]/20], [maxYawRom, maxYawRom]
-x3, y3 = [0, num[-1]/20], [minYawRom+robotAngle, minYawRom+robotAngle]
+x1, y1 = [0, num[-1]/60], [minYawRom, minYawRom]
+x2, y2 = [0, num[-1]/60], [maxYawRom, maxYawRom]
+x3, y3 = [0, num[-1]/60], [minYawRom+robotAngle, minYawRom+robotAngle]
 plt.plot(x1, y1, '--', label='Base')
 plt.plot(x2, y2, '--', label='Max')
 # plt.plot(x3, y3, '--', label='True')
